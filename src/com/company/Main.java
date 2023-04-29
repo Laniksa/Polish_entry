631e99cfdd2951b2eff924224b68cadc9a20f3e0
Регина <rmashkovtseva@gmail.com>
2023-04-29 20:35:34 +0300

    Попытка сделать все с помощью листов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите математическую запись");
+        String input = scan.nextLine();
+
+        List <String> oper = new ArrayList();
+        List <Integer> number = new ArrayList();
+
     }
 }
