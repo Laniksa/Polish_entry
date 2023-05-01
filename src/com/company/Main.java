33f3e898ef0775e2d4c9eb171e98f254c1fbaf19
Регина <rmashkovtseva@gmail.com>
2023-05-01 15:36:35 +0300

    Попытка сделать все с помощью листов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,38 @@
 package com.company;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
+    System.out.println("Введите математическую запись");
+    public static String input = scan.nextLine();
+
+    public static List <String> oper = new ArrayList();
+    public static List <Integer> number = new ArrayList();
+
+    public static String[] symbols = input.split(" ");
+
+    public static Boolean isNumeric(String [] str){
+        for(String n : str){
+            try{
+                Integer.parseInt(n);
+                return true;
+            }catch(Exception e){
+                return false;
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите математическую запись");
-        String input = scan.nextLine();
 
-        List <String> oper = new ArrayList();
-        List <Integer> number = new ArrayList();
+
+
+
+
 
     }
 }
